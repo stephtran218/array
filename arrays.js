bd32a5bb757d39c942ceb99aa318774780184752
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-10-30 08:50:43 -0700

    pushing new files

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var typesOfCandy = function (candy){
+    //1. Create a variable to hold the different candies.
+    //2. Creat an array
+    //3. Return the types of candy
+    
+    //Step 1:
+    let typesOfCandy = 0;
+
+
+    
+}
+var candy = ["twix", "snickers", "hersheys", "butterfingers"]
+push("almond joy");
+push("sweettarts")
+console.log (candy)
+//Step 2:
+function push(candyItem){
+    candy[candy.length] = candyItem;
+}
